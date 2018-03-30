a5ae92b7128f8f35e4381ba0485c08bb388ec9d5
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-03-29 20:55:23 -0700

    Update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+(function(w, d){
+	var b = d.getElementsByTagName('body')[0];
+	var s = d.createElement("script"); s.async = true;
+	var v = !("IntersectionObserver" in w) ? "8.7.1" : "10.5.2";
+	s.src = "https://cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/" + v + "/lazyload.min.js";
+	w.lazyLoadOptions = {}; // Your options here. See "recipes" for more information about async.
+	b.appendChild(s);
+}(window, document));
+
 (function ($) {
     "use strict";
 
